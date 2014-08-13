b11fec2a493f5e96eff0dac0e241eab206f3f2d1
Neil Kinnish <hello@neilkinnish.com>
2014-08-13 13:09:02 +0100

    testing

---
+++ raise.go
@@ -0,0 +1,22 @@
+package workers
+
+import (
+  "encoding/json"
+  "fmt"
+  "net/http"
+)
+
+func Raise(w http.ResponseWriter, req *http.Request) {
+  w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+  Configure(map[string]string {
+    "server": os.Getenv("REDIS_URL"),
+    "database": os.Getenv("REDIS_DATABASE"),
+    "pool": os.Getenv("REDIS_POOL"),
+    "process": os.Getenv("REDIS_PROCESS"),
+  })
+
+  Enqueue("plan-one", "Add", []int{1, 2})
+
+  fmt.Fprintln("Raised")
+}
--- workers.go
+++ workers.go
@@ -47,6 +47,7 @@ func Quit() {
 
 func StatsServer(port int) {
 	http.HandleFunc("/stats", Stats)
+	http.HandleFunc("/raise", Raise)
 
 	Logger.Println("Stats are available at", fmt.Sprint("http://localhost:", port, "/stats"))
 
