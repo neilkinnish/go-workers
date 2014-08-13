77876c5cd1ae64836b11eb906eaed95ab9fea9b4
Neil Kinnish <hello@neilkinnish.com>
2014-08-13 16:15:16 +0100

    updated

--- raise.go
+++ raise.go
@@ -10,17 +10,19 @@ import (
 func Raise(w http.ResponseWriter, req *http.Request) {
   w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
-  Configure(map[string]string {
+  value := FormValue("test")
+
+  /*Configure(map[string]string {
     "server": os.Getenv("REDIS_URL"),
     "database": os.Getenv("REDIS_DATABASE"),
     "pool": os.Getenv("REDIS_POOL"),
     "process": os.Getenv("REDIS_PROCESS"),
   })
 
-  Enqueue("plan-one", "Add", []int{1, 2})
+  Enqueue("plan-one", "Add", []int{1, 2})*/
 
   stats := map[string]interface{}{
-    "raised": true,
+    "raised": value,
   }
 
   body, _ := json.MarshalIndent(stats, "", "  ")
